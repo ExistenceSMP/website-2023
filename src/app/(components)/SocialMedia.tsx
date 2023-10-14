9757d9a77b4463e0efbd31d84a91ca0711aea9fd
Mallory Hayr <hey@igalaxy.dev>
2023-10-14 02:59:01 -0700

    Update SocialMedia.tsx

--- src/app/(components)/SocialMedia.tsx
+++ src/app/(components)/SocialMedia.tsx
@@ -96,6 +96,9 @@ interface Props {
 }
 
 function SocialMediaIcon({ icon, path, title, shiftHeld }: Props) {
+	const OLD_PATREON_PATH =
+		'M0 .48v23.04h4.22V.48zm15.385 0c-4.764 0-8.641 3.88-8.641 8.65 0 4.755 3.877 8.623 8.641 8.623 4.75 0 8.615-3.868 8.615-8.623C24 4.36 20.136.48 15.385.48z';
+
 	return (
 		<Link
 			style={{
@@ -112,7 +115,7 @@ function SocialMediaIcon({ icon, path, title, shiftHeld }: Props) {
 						icon.title != 'Patreon'
 							? icon.path
 							: shiftHeld
-							? 'M0 .48v23.04h4.22V.48zm15.385 0c-4.764 0-8.641 3.88-8.641 8.65 0 4.755 3.877 8.623 8.641 8.623 4.75 0 8.615-3.868 8.615-8.623C24 4.36 20.136.48 15.385.48z'
+							? OLD_PATREON_PATH
 							: icon.path
 					}
 					fill="#ffffff"
